5b2f9c2fe603012ff0380e167e29ea131c86df7d
madprgmr <madprgmr@eefd84c6-e9a9-11dd-8fb1-9960f2a117f8>
2009-01-26 21:56:17 +0000

    Minor API work

--- trunk/ 3630-sp09-rovio/src/RovioAPI.java
+++ trunk/ 3630-sp09-rovio/src/RovioAPI.java
@@ -1,4 +1,23 @@
+import java.io.*;
 
 public class RovioAPI {
+	private final RovioConnection connection;
 
+	public RovioAPI(final RovioConnection connection) {
+		this.connection = connection;
+	}
+	
+	public RovioConnection getConnection() {
+		return connection;
+	}
+	
+	public void manualDrive(int direction, int speed) {
+		final int action = 18;
+		try {
+			InputStream in = getConnection().open(	"rev.cgi", "Cmd", "nav", "action", action,
+													"drive", direction, "speed", speed);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
