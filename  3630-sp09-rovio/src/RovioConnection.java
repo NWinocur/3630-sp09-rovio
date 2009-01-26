55454bde73aa7cb0028ee11b0b2cce8afcb2abe0
Sean Hussey <madprgmr@users.noreply.github.com>
2009-01-26 16:11:58 +0000

    Initial commit.

---
+++  3630-sp09-rovio/src/RovioConnection.java
@@ -0,0 +1,95 @@
+import java.net.*;
+import java.io.*;
+
+/**
+ * Provides basic interactions with the Rovio's web server.
+ * @author Sean Hussey
+ */
+public class RovioConnection {
+	private String host;
+	
+	/**
+	 * Defines a connection the the Rovio robot.
+	 * @param host	the address of the Rovio's web server.
+	 */
+	public RovioConnection(final String host) {
+		setHost(host);
+	}
+	
+	/**
+	 * Defines a password-protected connection to the Rovio robot.
+	 * @param host	the address of the Rovio's web server.
+	 * @param userName	the name of the user.
+	 * @param password	the password for the given user.
+	 */
+	public RovioConnection(final String host, final String userName, final String password) {
+		this(host);
+
+		Authenticator.setDefault(new Authenticator() {
+			final PasswordAuthentication auth = new PasswordAuthentication(userName, password.toCharArray());
+			
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return auth;  
+			}
+		});		
+	}
+	
+	public void setHost(final String host) {
+		this.host = host;
+	}
+		
+	public String getHost() {
+		return host;
+	}
+	
+	/**
+	 * Opens the given URL for reading.
+	 * @param url	the page's URL.
+	 * @return		the server's response.
+	 * @throws IOException if the connection fails.
+	 */
+	public static InputStream open(URL url) throws IOException {
+		URLConnection connection = url.openConnection();
+				
+		connection.connect();
+		
+		return connection.getInputStream();
+	}
+	
+	/**
+	 * Opens the given resource on the Rovio for reading.
+	 * <br/>Equivalent to calling <code>open(new URL("http",getHost(),resourceName))</code>.
+	 * @param resourceName	the name of the resource to read from. 
+	 * @return
+	 */
+	public InputStream open(String resourceName) throws IOException {
+		try {
+			return open(new URL("http", host, resourceName));
+		} catch(MalformedURLException e) {
+			throw new IOException(e);
+		}
+	}
+	
+	// Debugging/test main method.
+	public static void main(String[] args) {
+		// Create a password-protected connection to the robot
+		RovioConnection rovio = new RovioConnection("http://3630rovio.servebeer.com", "admin", "3630class");
+		
+		try {
+			// Query the robot's status
+			URL url = new URL(rovio.getHost() + "/rev.cgi?Cmd=nav&action=1");
+			InputStream in = rovio.open(url);
+			
+			// Display the result
+			// Note: Use a Scanner to process this.
+			int c = in.read();
+			while(c >= 0) {
+				System.out.print((char)c);
+				c = in.read();
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
