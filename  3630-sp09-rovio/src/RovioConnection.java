e8f104b5684e1ea463cb28d5f90e928e8a3a77fa
Sean Hussey <madprgmr@users.noreply.github.com>
2009-01-26 21:56:17 +0000

    Minor API work

---  3630-sp09-rovio/src/RovioConnection.java
+++  3630-sp09-rovio/src/RovioConnection.java
@@ -59,26 +59,47 @@ public class RovioConnection {
 	/**
 	 * Opens the given resource on the Rovio for reading.
 	 * <br/>Equivalent to calling <code>open(new URL("http",getHost(),resourceName))</code>.
-	 * @param resourceName	the name of the resource to read from. 
+	 * @param resource	the resource to read from. 
 	 * @return
 	 */
-	public InputStream open(String resourceName) throws IOException {
+	public InputStream open(String resource) throws IOException {
+		// Ensure that the file is prefixed with a backslash
+		if(!resource.startsWith("//"))
+			resource = "//" + resource;
+		
 		try {
-			return open(new URL("http", host, resourceName));
+			return open(new URL("http", host, resource));
 		} catch(MalformedURLException e) {
 			throw new IOException(e);
 		}
 	}
 	
+	public InputStream open(String resource, Object... args) throws IOException {
+		// Ensure early failure if odd number of argument name/value pairings
+		if(args.length % 2 != 0)
+			throw new IOException(new IllegalArgumentException("Every argument name must have an associated value"));
+		
+		// Append the arguments to the end of the resource
+		for(int i = 0; i < args.length; i += 2) {
+			resource += (i == 0 ? "?" : "&") + args[i] + "=" + args[i + 1];
+		}
+		
+		return open(resource);
+	}
+	
 	// Debugging/test main method.
 	public static void main(String[] args) {
 		// Create a password-protected connection to the robot
-		RovioConnection rovio = new RovioConnection("http://3630rovio.servebeer.com", "admin", "3630class");
+		RovioConnection rovio; 
+//		rovio = new RovioConnection("http://3630rovio.servebeer.com", "admin", "3630class");
+		rovio = new RovioConnection("192.168.10.18");
 		
 		try {
 			// Query the robot's status
-			URL url = new URL(rovio.getHost() + "/rev.cgi?Cmd=nav&action=1");
-			InputStream in = rovio.open(url);
+			InputStream in;
+//			URL url = new URL(rovio.getHost() + "/rev.cgi?Cmd=nav&action=1");
+//			in = rovio.open(url);
+			in = rovio.open("rev.cgi", "Cmd", "nav", "action", 1);
 			
 			// Display the result
 			// Note: Use a Scanner to process this.
---  3630-sp09-rovio/src/RovioConstants.java
+++  3630-sp09-rovio/src/RovioConstants.java
@@ -1,4 +1,62 @@
 
 public interface RovioConstants {
 
+	public static enum RovioState {
+		IDLE, 
+		DRIVING_HOME,
+		DOCKING,
+		EXECUTING_PATH,
+		RECORDING_PATH
+	}
+	
+	public static enum RovioStateFlag {
+		HOME_POSITION (1);
+		
+		private final int value;
+		
+		RovioStateFlag(int value) {
+			this.value = value;
+		}
+		
+		public RovioStateFlag[] getFlags(int value) {
+			int numFlags = 0;
+			int a = value;
+			while(a > 0) {
+				if((a & 1) == 1)
+					numFlags++;
+				a = a >> 1;
+			}
+			
+			RovioStateFlag[] flags = new RovioStateFlag[numFlags];
+			
+			for(RovioStateFlag flag : RovioStateFlag.values()) {
+				if((flag.value & value) > 0) {
+					
+				}
+			}
+			
+			return flags;
+		}
+	}
+	
+	public static enum CameraResolution {
+		_176x144,
+		_320x240,
+		_352x240,
+		_640x480
+	}
+	
+	public static enum VideoCompression {
+		LOW,
+		MEDIUM,
+		HIGH
+	}
+	
+	public static enum DDNSState {
+		NO_UPDATE,
+		UPDATING,
+		UPDATE_SUCCESSFUL,
+		UPDATE_FAILED
+	}
+	
 }
