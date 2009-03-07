9ddd53d67e66558fb621138aa6e4497bc64cc214
Nicolas Winocur <NWinocur@users.noreply.github.com>
2009-03-07 00:29:18 +0000

    Basic image acquisition and noise reduction completed via image averaging.  Convolution method still in there since it'll likely still be useful

---  3630-sp09-rovio/src/ooPurtyColors.java
+++  3630-sp09-rovio/src/ooPurtyColors.java
@@ -2,6 +2,7 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
+import java.awt.image.WritableRaster;
 
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
@@ -11,6 +12,7 @@ public class ooPurtyColors extends Planner {
 	public RovioConstants.CameraResolution cameraResolution;
 	public int cameraBrightness;
 	public RovioConstants.HeadPosition headPosition;
+	public final int burstLength = 4;
 
 	public final float[] horizLineDetect = { -1, -2, -1, 0, 0, 0, 1, 2, 1 };
 	public final float[] interestPointDetector = { -1, -1, -1, -1, 8, -1, -1,
@@ -31,20 +33,70 @@ public class ooPurtyColors extends Planner {
 	 */
 	@Override
 	public void makeMove() {
-		BufferedImage rawImage = super.robot.whatDoISee(cameraResolution); // get
-																		// image
-		showImage(rawImage);
-		
+		BufferedImage rawImage[] = burstFire(burstLength);
 		BufferedImage noiseReduced = reduceNoise(rawImage);
 		showImage(noiseReduced);
+		
 		// segment image
 		// image description (features)
 		// recognition/extraction
 	}
 	
+	private BufferedImage[] burstFire(int imagesToTake) {
+		BufferedImage imagesToReturn[] = new BufferedImage[imagesToTake];
+		for (int n = 0; n < imagesToTake; n++){
+			imagesToReturn[n] = super.robot.whatDoISee(cameraResolution);
+			RovioAPI.napTime(5);
+		}
+		return imagesToReturn;
+		
+	}
+	
+	private BufferedImage reduceNoise(BufferedImage[] rawImage) {
+		return average(rawImage);
+	}
+
 	private BufferedImage reduceNoise(BufferedImage rawImage) {
 		return rawImage;// making this a do-nothing method for now
 	}
+	
+	 public static BufferedImage average(BufferedImage[] images) {
+
+		int n = images.length;
+
+		// Assuming that all images have the same dimensions
+		int w = images[0].getWidth();
+		int h = images[0].getHeight();
+
+
+		BufferedImage average = new BufferedImage(w, h,
+				BufferedImage.TYPE_INT_RGB);
+
+		WritableRaster raster = average.getRaster()
+				.createCompatibleWritableRaster();
+
+		for (int y = 0; y < h; ++y)
+			for (int x = 0; x < w; ++x) {
+
+				float rsum = 0.0f;
+				float gsum = 0.0f;
+				float bsum = 0.0f;
+
+				for (int i = 0; i < n; ++i) {
+					rsum = rsum + images[i].getRaster().getSample(x, y, 0);
+					gsum = gsum + images[i].getRaster().getSample(x, y, 1);
+					bsum = bsum + images[i].getRaster().getSample(x, y, 2);
+				}
+				raster.setSample(x, y, 0, Math.round(rsum / n));
+				raster.setSample(x, y, 1, Math.round(gsum / n));
+				raster.setSample(x, y, 2, Math.round(bsum / n));
+
+			}
+
+		average.setData(raster);
+
+		return average;
+	} 
 
 	// ASSUMES SQUARE KERNEL
 	private BufferedImage convolveBuffWithKernel(BufferedImage sourceImage,
