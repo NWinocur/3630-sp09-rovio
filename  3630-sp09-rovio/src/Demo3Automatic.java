98df6e38746a458e967c75520526edceb14ea2d6
jon180 <jon180@gmail.com>
2009-04-18 18:41:00 +0000

    wrote Demo3Automatic and implemented it into Demo3Manual

---
+++  3630-sp09-rovio/src/Demo3Automatic.java
@@ -0,0 +1,33 @@
+import java.awt.image.BufferedImage;
+
+public class Demo3Automatic extends Planner {
+	
+	private ColorSpace cspace;
+	private Map map;
+	
+	public Demo3Automatic(Robot robot, ColorSpace cspace Map map) {
+		super(robot);
+		this.cspace = cspace;
+		this.map = map;
+	}
+	
+	public void makeMove() {
+		Waypoint currentLocation = null;
+		Waypoint goal = new Waypoint(0, 0, 0);
+		// localize
+		currentLocation = localize();
+		// go to goal
+		driveToGoal(currentLocation, goal);
+		// halt
+		while (true) {}
+	}
+	
+	public Waypoint localize() {
+		return new Waypoint(0, 0, 0);
+	}
+	
+	public void driveToGoal(Waypoint current, Waypoint goal) {
+		
+	}
+	
+}
