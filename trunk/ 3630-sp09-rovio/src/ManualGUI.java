76c915d331c911b55abdad26d61d41b3edde52cc
jon180 <jon180@eefd84c6-e9a9-11dd-8fb1-9960f2a117f8>
2009-04-19 18:09:42 +0000

    added timed delay, image preview, and status to GUI

--- trunk/ 3630-sp09-rovio/src/ManualGUI.java
+++ trunk/ 3630-sp09-rovio/src/ManualGUI.java
@@ -1,12 +1,20 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import javax.swing.*;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ManualGUI {
+	
+	/** time in miliseconds to ignore keyboard and button events after an event */
+	public static final long INPUT_DELAY = 300;
 	
 	private Demo3Manual planner;
 	private ButtonListener bl;
 	private KeyBoardListener kl;
+	private boolean isAsleep;
 	
 	private JButton mapStopButton;
 	private JButton autoModeButton;
@@ -19,6 +27,7 @@ public class ManualGUI {
 		this.planner = planner;
 		this.bl = new ButtonListener();
 		this.kl = new KeyBoardListener();
+		this.isAsleep = false;
 		JPanel panel = this.makeMainPanel();
 		JFrame frame = new JFrame("Manual Robot Control");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -35,16 +44,15 @@ public class ManualGUI {
 	/** use this to see a GUI mockup */
 	public static void main(String[] args) {
 		ManualGUI g = new ManualGUI(null);
-		g.setStatus("testing");
 	}
 	
 	public JPanel makeMainPanel() {
 		JPanel panel = new JPanel();
-		panel.setPreferredSize(new Dimension(400, 300));
 		panel.setLayout(new BorderLayout());
 		panel.add(this.north(), BorderLayout.NORTH);
 		panel.add(this.south(), BorderLayout.SOUTH);
 		panel.add(this.center(), BorderLayout.CENTER);
+		panel.add(this.west(), BorderLayout.WEST);
 		return panel;
 	}
 	
@@ -65,7 +73,8 @@ public class ManualGUI {
 	
 	public JPanel center() {
 		JPanel panel = new JPanel();
-		this.status = new JLabel("status: ");
+		panel.setPreferredSize(new Dimension(300, 480));
+		this.status = new JLabel("status: waiting for command");
 		panel.add(this.status);
 		return panel;
 	}
@@ -90,18 +99,58 @@ public class ManualGUI {
 		return panel;
 	}
 	
+	public JPanel west() {
+		return new ImagePreview();
+	}
+	
+	public class ImagePreview extends JPanel {
+		private BufferedImage canvas;
+		public ImagePreview() {
+			setPreferredSize(new Dimension(640, 480));
+			canvas = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
+		}
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			Graphics2D canvas2 = (Graphics2D) g;
+			canvas2.drawImage(canvas, null, 0, 0);
+		}
+		public void setCanvas(BufferedImage bi) {
+			canvas = bi;
+		}
+	}
+	
+	private class InputDelay extends TimerTask {
+		public void run() {
+			isAsleep = false;
+			setStatus("waiting for command");
+		}
+	}
+	
 	private class ButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			if (e.getActionCommand().equals("driveForward")) {
-				planner.driveForward();
-			} else if (e.getActionCommand().equals("turnLeft")) {
-				planner.turnLeft();
-			} else if (e.getActionCommand().equals("turnRight")) {
-				planner.turnRight();
-			} else if (e.getActionCommand().equals("mapStop")) {
-				//planner.mapStop();
-			} else if (e.getActionCommand().equals("autoMode")) {
-				//planner.autoMode();
+			if (isAsleep == false) {
+				isAsleep = true;
+				Timer t = new Timer();
+				t.schedule(new InputDelay(), INPUT_DELAY);
+				System.out.println("button event: " + e.getActionCommand());
+				if (e.getActionCommand().equals("driveForward")) {
+					setStatus("driving forward");
+					planner.driveForward();
+				} else if (e.getActionCommand().equals("turnLeft")) {
+					setStatus("turning left");
+					planner.turnLeft();
+				} else if (e.getActionCommand().equals("turnRight")) {
+					setStatus("turning right");
+					planner.turnRight();
+				} else if (e.getActionCommand().equals("mapStop")) {
+					setStatus("map stop");
+					//planner.mapStop();
+				} else if (e.getActionCommand().equals("autoMode")) {
+					setStatus("automatic mode");
+					//planner.autoMode();
+					// sleep forever (ignore GUI inputs)
+					isAsleep = true;
+				}
 			}
 		}
 	}
@@ -110,15 +159,24 @@ public class ManualGUI {
 		public void keyPressed(KeyEvent e) {}
 		public void keyTyped(KeyEvent e) {}
 		public void keyReleased(KeyEvent e) {
-			System.out.println(String.format("keyboard event: %d\n", e.getKeyCode()));
-			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-				planner.turnLeft();
-			} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-				planner.turnRight();
-			} else if (e.getKeyCode() == KeyEvent.VK_UP) {
-				planner.driveForward();
-			} else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-				//planner.mapStop();
+			if (isAsleep == false) {
+				isAsleep = true;
+				Timer t = new Timer();
+				t.schedule(new InputDelay(), INPUT_DELAY);
+				System.out.println(String.format("keyboard event: %d\n", e.getKeyCode()));
+				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+					setStatus("turning left");
+					planner.turnLeft();
+				} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+					setStatus("turning right");
+					planner.turnRight();
+				} else if (e.getKeyCode() == KeyEvent.VK_UP) {
+					setStatus("driving forward");
+					planner.driveForward();
+				} else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					setStatus("map stop");
+					//planner.mapStop();
+				}
 			}
 		}
 	}
